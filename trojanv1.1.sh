7db411c15c7286a8e58023c8cae41c229a5a4920
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-14 13:50:48 +0800

    Update trojanv1.1.sh

--- trojanv1.1.sh
+++ trojanv1.1.sh
@@ -410,12 +410,12 @@ server {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:10000;
         proxy_http_version 1.1;
-        proxy_set_header Early-Data $ssl_early_data;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Early-Data '$ssl_early_data';
+        proxy_set_header Upgrade '$http_upgrade';
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $http_host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host '$http_host';
+        proxy_set_header X-Real-IP '$remote_addr';
+        proxy_set_header X-Forwarded-For '$proxy_add_x_forwarded_for';
         error_page 400 = @errpage;
         }
         location @errpage {
