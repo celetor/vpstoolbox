b9420f6df75ad9dcbce0233c7e9c010b80ac9f12
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-14 13:43:22 +0800

    Update trojanv1.1.sh

--- trojanv1.1.sh
+++ trojanv1.1.sh
@@ -257,23 +257,6 @@ server {
       root /usr/share/nginx/html/;
         index index.html;
         }
-    location /ray { #V2ray Websocket Plugin
-        access_log off;
-        proxy_intercept_errors on;
-        proxy_redirect off;
-        proxy_pass http://127.0.0.1:10000;
-        proxy_http_version 1.1;
-        proxy_set_header Early-Data $ssl_early_data;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $http_host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        error_page 400 = @errpage;
-        }
-        location @errpage {
-        return 403;
-        }
   add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
 }
 
