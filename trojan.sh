7d5e339ab9eeb3bfe13583da2225b1fa6292f7e2
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-17 13:55:01 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -171,6 +171,7 @@ nginxyum(){
 nginxapt(){
 	wget https://nginx.org/keys/nginx_signing.key
 	apt-key add nginx_signing.key
+        rm -rf nginx_signing.key
   touch /etc/apt/sources.list.d/nginx.list
   cat > '/etc/apt/sources.list.d/nginx.list' << EOF
 deb https://nginx.org/packages/mainline/debian/ $(lsb_release -cs) nginx
@@ -183,6 +184,7 @@ EOF
 nginxubuntu(){
 	wget https://nginx.org/keys/nginx_signing.key
 	apt-key add nginx_signing.key
+        rm -rf nginx_signing.key
   touch /etc/apt/sources.list.d/nginx.list
   cat > '/etc/apt/sources.list.d/nginx.list' << EOF
 deb https://nginx.org/packages/mainline/ubuntu/ $(lsb_release -cs) nginx
