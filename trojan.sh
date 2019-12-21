04e43089f87a7dd7dbc4833ddeeee096111bf38d
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-21 14:06:22 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -148,7 +148,7 @@ installdependency(){
     yum install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils python3-qrcode python-pil unzip resolvconf -qq -y
  elif [[ $dist = ubuntu ]]; then
     apt-get install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils lsb-release python-pil unzip resolvconf -qq -y
-    if [[ $(lsb_release -cs) == xenial ]]; then
+    if [[ $(lsb_release -cs) == xenial ]] || [[ $(lsb_release -cs) == trusty ]]; then
       colorEcho ${ERROR} "Ubuntu 16.04 does not support python3-qrcode,Skipping generating QR code!"
       else
         apt-get install python3-qrcode -qq -y
