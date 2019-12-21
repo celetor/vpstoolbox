b8d1afaef8b2b572c9020be75679de43f1d8d8e0
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-21 13:52:02 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -107,14 +107,10 @@ installdependency(){
     apt-get install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils lsb-release python-pil unzip resolvconf -qq -y
     if [[ $(lsb_release -cs) == xenial ]]; then
     	TERM=ansi whiptail --title "Skipping generating QR code!" --infobox "Ubuntu 16.04 does not support python3-qrcode,Skipping generating QR code!" 8 78
-      else
-        apt-get install python3-qrcode -qq -y
-    fi
-    if [[ $(lsb_release -cs) == trusty ]]; then
+	elif [[ $(lsb_release -cs) == trusty ]]; then
       colorEcho ${ERROR} "Ubuntu 14.04 does not support python3-qrcode,Skipping generating QR code!"
       else
         apt-get install python3-qrcode -qq -y
-    fi
  elif [[ $dist = debian ]]; then
     apt-get install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils lsb-release python3-qrcode python-pil unzip resolvconf -qq -y
  else
