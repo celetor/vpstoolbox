ca06b65647af74dfd414d7a4b0655de8d844c5fd
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-23 19:00:48 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -783,8 +783,7 @@ installv2ray(){
         "domain": [
         "baidu.com",
         "qq.com",
-        "sina.com",
-        "geosite:cn"
+        "sina.com"
       ],
       "outboundTag": "blocked"
       }
