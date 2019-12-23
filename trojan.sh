375728b705a9a3b49832caf3104093100e346134
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-23 09:47:36 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -145,7 +145,7 @@ openfirewall(){
 installdependency(){
   echo "installing trojan-gfw nginx and acme"
   if [[ $dist = centos ]]; then
-    yum install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils python3-qrcode unzip -qq -y
+    yum install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 python3-qrcode unzip -qq -y
  elif [[ $dist = ubuntu ]]; then
     apt-get install sudo curl socat xz-utils wget apt-transport-https gnupg gnupg2 dnsutils lsb-release python-pil unzip resolvconf -qq -y
     if [[ $(lsb_release -cs) == xenial ]] || [[ $(lsb_release -cs) == trusty ]]; then
