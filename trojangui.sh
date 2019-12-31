c1299fc5fb29d459a9e2149da22fa3d6de948da1
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2019-12-31 12:47:56 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -860,11 +860,9 @@ server {
         }
     location $path {
         access_log off;
-        proxy_intercept_errors on;
         proxy_redirect off;
         proxy_pass http://127.0.0.1:10000;
         proxy_http_version 1.1;
-        proxy_set_header Early-Data @ssl_early_data;
         proxy_set_header Upgrade @http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host @http_host;
