e37103c7649309ae2239be75c4b69595804b5a50
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-01 13:13:25 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -274,9 +274,6 @@ server {
     listen       80;
     server_name  $domain;
 
-    #charset koi8-r;
-    #access_log  /var/log/nginx/host.access.log  main;
-
     location / {
         root   /usr/share/nginx/html;
         index  index.html index.htm;
@@ -290,29 +287,6 @@ server {
     location = /50x.html {
         root   /usr/share/nginx/html;
     }
-
-    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
-    #
-    #location ~ \.php$ {
-    #    proxy_pass   http://127.0.0.1;
-    #}
-
-    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-    #
-    #location ~ \.php$ {
-    #    root           html;
-    #    fastcgi_pass   127.0.0.1:9000;
-    #    fastcgi_index  index.php;
-    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
-    #    include        fastcgi_params;
-    #}
-
-    # deny access to .htaccess files, if Apache's document root
-    # concurs with nginx's one
-    #
-    #location ~ /\.ht {
-    #    deny  all;
-    #}
 }
 EOF
   systemctl start nginx
@@ -756,7 +730,7 @@ systemctl daemon-reload
 ##########iptables-persistent########
 iptables-persistent(){
   if [[ $dist = centos ]]; then
-    yum install iptables-persistent -q -y > /dev/null
+    :
  elif [[ $dist = ubuntu ]]; then
     export DEBIAN_FRONTEND=noninteractive
     apt-get install iptables-persistent -q -y > /dev/null
@@ -1205,6 +1179,10 @@ checkupdate(){
 ###########Trojan share link########
 trojanlink(){
   cd
+  colorEcho ${INFO} "Your Trojan-Gfw Share link1 is"
+  colorEcho ${LINK} "trojan://$password1@$domain:443"
+  colorEcho ${INFO} "Your Trojan-Gfw Share link2 is"
+  colorEcho ${LINK} "trojan://$password2@$domain:443"
   if [[ $(lsb_release -cs) != xenial ]] || [[ $(lsb_release -cs) == trusty ]] || [[ $(lsb_release -cs) == jessie ]]; then
   wget https://github.com/trojan-gfw/trojan-url/raw/master/trojan-url.py -q
   chmod +x trojan-url.py
@@ -1213,10 +1191,6 @@ trojanlink(){
   ./trojan-url.py -q -i /etc/trojan/client2.json -o $password2.png
   cp $password1.png /usr/share/nginx/html/
   cp $password2.png /usr/share/nginx/html/
-  colorEcho ${INFO} "Your Trojan-Gfw Share link1 is"
-  colorEcho ${LINK} "trojan://$password1@$domain:443"
-  colorEcho ${INFO} "Your Trojan-Gfw Share link2 is"
-  colorEcho ${LINK} "trojan://$password2@$domain:443"
   colorEcho ${INFO} "Please visit the link below to get your QR code1"
   colorEcho ${LINK} "https://$domain/$password1.png"
   colorEcho ${INFO} "Please visit the link below to get your QR code2"
@@ -1225,7 +1199,7 @@ trojanlink(){
   rm -rf $password1.png
   rm -rf $password2.png
   else
-    colorEcho ${ERROR} "QR generate Fail ! Because Ubuntu 16.04 does not support python3-qrcode,Please change your os!"
+    colorEcho ${ERROR} "QR generate Fail ! Because your os does not support python3-qrcode,Please consider change your os!"
   fi
 }
 ########V2ray share link############
