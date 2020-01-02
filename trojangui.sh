ce70090f59b60c52a9a9600fc8053dfd8ac33709
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-02 16:06:41 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -1368,6 +1368,8 @@ function advancedMenu() {
         colorEcho ${INFO} "你的 os codename is $dist $(lsb_release -cs)"
         colorEcho ${INFO} "Updating system"
         updatesystem
+        colorEcho ${INFO} "安装 dependency"
+        installdependency
         if [[ $system_upgrade = 1 ]]; then
     upgradesystem
     else
@@ -1379,8 +1381,6 @@ function advancedMenu() {
     :
     fi
         clear
-        colorEcho ${INFO} "安装 dependency"
-        installdependency
         if isresolved $domain
         then
         :
