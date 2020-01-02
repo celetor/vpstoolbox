46e7c7f4b6df59ec99a53e1269e4a4ce18ebfe05
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-02 15:42:57 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -1050,7 +1050,7 @@ v2rayclient(){
               {
                 "id": "$uuid",
                 "alterId": 64,
-                "security": "none" //使用TLS则无需二次加密
+                "security": "auto"
               }
             ]
           }
