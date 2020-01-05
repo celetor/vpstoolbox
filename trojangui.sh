0a094076080172af60bbf540f39bae6607e673e0
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-05 22:13:14 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -1279,10 +1279,6 @@ html(){
   unzip -o $htmlcode.zip -d /usr/share/nginx/html/
   rm -rf $htmlcode.zip
 }
-#########Cron Job#################
-cronjob(){
-  (crontab -l && echo "30 03 01 */2 * systemctl restart trojan") | crontab -
-}
 ##################################
 timesync(){
   timedatectl set-timezone Asia/Hong_Kong
@@ -1374,7 +1370,6 @@ function advancedMenu() {
         clear
         colorEcho ${INFO} "启动 trojan-gfw and nginx | setting up boot autostart"
         autostart
-        #cronjob
         timesync
         clear
         trojanclient
@@ -1453,7 +1448,6 @@ function advancedMenu() {
         installv2ray
         colorEcho ${INFO} "starting trojan-gfw v2ray and nginx | setting up boot autostart"
         autostart
-        #cronjob
         timesync
         clear
         trojanclient
