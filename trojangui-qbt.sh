6570b725435b428a908c5182c7b2f556c2be0d8b
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-06 10:30:38 +0800

    Update trojangui-qbt.sh

--- trojangui-qbt.sh
+++ trojangui-qbt.sh
@@ -467,11 +467,11 @@ server {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:9000;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade @http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host @http_host;
-        proxy_set_header X-Real-IP @remote_addr;
-        proxy_set_header X-Forwarded-For @proxy_add_x_forwarded_for;
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         }
   add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
 }
