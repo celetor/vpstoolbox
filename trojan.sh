4e716e6029b500e5bca17a6e559d45ee58008362
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-06 14:02:06 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -171,6 +171,20 @@ openfirewall(){
   ip6tables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
   ip6tables -I OUTPUT -j ACCEPT
     if [[ $dist = centos ]]; then
+        setenforce 0
+          cat > '/etc/selinux/config' << EOF
+# This file controls the state of SELinux on the system.
+# SELINUX= can take one of these three values:
+#     enforcing - SELinux security policy is enforced.
+#     permissive - SELinux prints warnings instead of enforcing.
+#     disabled - No SELinux policy is loaded.
+SELINUX=disabled
+# SELINUXTYPE= can take one of these three values:
+#     targeted - Targeted processes are protected,
+#     minimum - Modification of targeted policy. Only selected processes are protected. 
+#     mls - Multi Level Security protection.
+SELINUXTYPE=targeted
+EOF
     firewall-cmd --zone=public --add-port=80/tcp --permanent
     firewall-cmd --zone=public --add-port=443/tcp --permanent
     systemctl stop firewalld
