1b727d9c7ee9648b0240753609398fe16029d756
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-06 10:18:52 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -271,9 +271,9 @@ http {
   access_log /var/log/nginx/access.log;
 
 
-  log_format  main  '@remote_addr - @remote_user [$time_local] "@request" '
-    '@status $body_bytes_sent "@http_referer" '
-    '"@http_user_agent" "@http_x_forwarded_for"';
+  log_format  main  '\$remote_addr - \$remote_user [$time_local] "\$request" '
+    '\$status $body_bytes_sent "\$http_referer" '
+    '"\$http_user_agent" "\$http_x_forwarded_for"';
 
   sendfile on;
   gzip on;
