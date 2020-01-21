375db415c2e49b4a1264f1223a42faee92a16c67
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-01-21 20:48:51 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -1439,7 +1439,7 @@ trojanclient(){
         "no_delay": true,
         "keep_alive": true,
         "reuse_port": false,
-        "fast_open": true,
+        "fast_open": false,
         "fast_open_qlen": 20
     }
 }
@@ -1474,7 +1474,7 @@ EOF
         "no_delay": true,
         "keep_alive": true,
         "reuse_port": false,
-        "fast_open": true,
+        "fast_open": false,
         "fast_open_qlen": 20
     }
 }
