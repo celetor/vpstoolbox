e7066bf35d9146de3adf46864e4176fc177d8810
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-02-02 21:26:14 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -395,12 +395,12 @@ set -e
     dist=ubuntu
     pack="apt-get -y -qq"
     apt-get update
-    apt-get install lsb_release -y -qq > /dev/null || true
+    apt-get install lsb-release -y -qq > /dev/null || true
  elif cat /etc/*release | grep ^NAME | grep -q Debian; then
     dist=debian
     pack="apt-get -y -qq"
     apt-get update
-    apt-get install lsb_release -y -qq > /dev/null || true
+    apt-get install lsb-release -y -qq > /dev/null || true
  else
   TERM=ansi whiptail --title "OS SUPPORT" --infobox "OS NOT SUPPORTED, couldn't install Trojan-gfw" 8 78
     exit 1;
