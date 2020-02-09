96a2b204b70ba50ee588eab220b1b903f5b864a0
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-02-09 11:00:23 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#MIT License
+#
+#Copyright (c) 2019-2020 johnrosen1
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
 clear
 
 if [[ $(id -u) != 0 ]]; then
@@ -2522,7 +2544,7 @@ bandwithusage(){
 ##################################
 clear
 function advancedMenu() {
-		ADVSEL=$(whiptail --clear --ok-button "吾意已決 立即安排" --title "Trojan-Gfw Script Menu" --menu --nocancel "Choose an option RTFM: https://www.johnrosen1.com/trojan/
+		ADVSEL=$(whiptail --clear --ok-button "吾意已決 立即安排" --title "VPS ToolBox Script Menu" --menu --nocancel "Choose an option:
 运行此脚本前请在控制面板中开启80 443端口并关闭Cloudflare CDN!" 13 78 4 \
 				"1" "安裝(Install)" \
 				"2" "结果(Result)" \
