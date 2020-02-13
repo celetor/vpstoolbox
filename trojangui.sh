ec40fec34883ed0c47ff181914a554356dabf50c
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2020-02-13 19:02:45 +0800

    Update trojangui.sh

--- trojangui.sh
+++ trojangui.sh
@@ -111,7 +111,7 @@ WARNING="33m"   # Warning message
 INFO="36m"     # Info message
 LINK="92m"     # Share Link Message
 #############################
-cipher_server="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384"
+cipher_server="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384"
 cipher_client="ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA"
 #############################
 setlanguage(){
@@ -232,7 +232,15 @@ server {
 EOF
 	nginx -t
 	systemctl start nginx || true
-	~/.acme.sh/acme.sh --issue --nginx -d $domain -k ec-256 --force --log --reloadcmd "systemctl reload trojan || true && nginx -s reload"  
+	colorEcho ${INFO} "测试证书申请ing(test issuing) let\'s encrypt certificate"
+	~/.acme.sh/acme.sh --issue --nginx -d $domain -k ec-256 --force --test --log --reloadcmd "systemctl reload trojan || true && nginx -s reload"
+	if [[ $? != 0 ]]; then
+	colorEcho ${ERROR} "证书申请测试失败，请检查VPS控制面板防火墙(80 443)是否打开!!!"
+	colorEcho ${ERROR} "Domain verification fail,Pleae Open port 80 443 on VPS panel !!!"
+	fi 
+	clear
+	colorEcho ${INFO} "正式证书申请ing(issuing) let\'s encrypt certificate"
+	~/.acme.sh/acme.sh --issue --nginx -d $domain -k ec-256 --force --log --reloadcmd "systemctl reload trojan || true && nginx -s reload"
 	~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/trojan/trojan.crt --keypath /etc/trojan/trojan.key --ecc
 	chmod +r /etc/trojan/trojan.key
 	fi
@@ -425,13 +433,13 @@ domain=$(whiptail --inputbox --nocancel "快輸入你的域名並按回車" 8 78
 done
 if [[ $install_trojan = 1 ]]; then
 	while [[ -z $password1 ]]; do
-password1=$(whiptail --passwordbox --nocancel "快輸入你想要的Trojan-GFW密碼一併按回車" 8 78 --title "password1 input" 3>&1 1>&2 2>&3)
+password1=$(whiptail --passwordbox --nocancel "快輸入你想要的Trojan-GFW密碼一併按回車(若不確定，請直接回車，会随机生成)" 8 78 --title "password1 input" 3>&1 1>&2 2>&3)
 if [[ $password1 == "" ]]; then
 	password1=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '' )
 	fi
 done
 while [[ -z $password2 ]]; do
-password2=$(whiptail --passwordbox --nocancel "快輸入想要的Trojan-GFW密碼二並按回車" 8 78 --title "password2 input" 3>&1 1>&2 2>&3)
+password2=$(whiptail --passwordbox --nocancel "快輸入想要的Trojan-GFW密碼二並按回車(若不確定，請直接回車，会随机生成)" 8 78 --title "password2 input" 3>&1 1>&2 2>&3)
 if [[ $password2 == "" ]]; then
 	password2=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20 ; echo '' )
 	fi
@@ -500,7 +508,7 @@ done
 			sspath=$(whiptail --inputbox --nocancel "快输入你的想要的ss-Websocket路径并按回车" 8 78 /ss --title "ss-Websocket path input" 3>&1 1>&2 2>&3)
 			done
 			while [[ -z $sspasswd ]]; do
-			sspasswd=$(whiptail --passwordbox --nocancel "快输入你的想要的ss密码并按回车" 8 78  --title "ss passwd input" 3>&1 1>&2 2>&3)
+			sspasswd=$(whiptail --passwordbox --nocancel "快输入你的想要的ss密码并按回车(若不確定，請直接回車，会随机生成)" 8 78  --title "ss passwd input" 3>&1 1>&2 2>&3)
 			if [[ $sspasswd == "" ]]; then
 			sspasswd=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 40 ; echo '' )
 			fi
@@ -1789,56 +1797,56 @@ if [[ $install_trojan = 1 ]]; then
 	cat > '/etc/nginx/conf.d/trojan.conf' << EOF
 server {
 	listen 127.0.0.1:80;
-		server_name $domain;
-		if (\$http_user_agent = "") { return 444; }
-		if (\$host != "$domain") { return 404; }
-		add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
-		add_header X-Frame-Options SAMEORIGIN always;
-		add_header X-Content-Type-Options "nosniff" always;
-		add_header Referrer-Policy "no-referrer";
-		#add_header Content-Security-Policy "default-src 'self'; script-src 'self' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
-		add_header Feature-Policy "geolocation none;midi none;notifications none;push none;sync-xhr none;microphone none;camera none;magnetometer none;gyroscope none;speaker self;vibrate none;fullscreen self;payment none;";
-		location / {
-			root /usr/share/nginx/html/;
-				index index.html;
-				}
+	server_name $domain;
+	if (\$http_user_agent = "") { return 444; }
+	if (\$host != "$domain") { return 404; }
+	add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+	add_header X-Frame-Options SAMEORIGIN always;
+	add_header X-Content-Type-Options "nosniff" always;
+	add_header Referrer-Policy "no-referrer";
+	#add_header Content-Security-Policy "default-src 'self'; script-src 'self' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
+	add_header Feature-Policy "geolocation none;midi none;notifications none;push none;sync-xhr none;microphone none;camera none;magnetometer none;gyroscope none;speaker self;vibrate none;fullscreen self;payment none;";
+	location / {
+		root /usr/share/nginx/html/;
+			index index.html;
+		}
 EOF
 	else
 	cat > '/etc/nginx/conf.d/trojan.conf' << EOF
 server {
-		listen 443 ssl http2;
-		listen [::]:443 ssl http2;
+	listen 443 ssl http2;
+	listen [::]:443 ssl http2;
 
-		ssl_certificate       /etc/trojan/$cert;
-		ssl_certificate_key   /etc/trojan/$cert_key;
-		ssl_protocols         TLSv1.3 TLSv1.2;
-		ssl_ciphers $cipher_server;
-		ssl_prefer_server_ciphers off;
-		ssl_early_data on;
-		ssl_session_cache   shared:SSL:40m;
-		ssl_session_timeout 1d;
-		ssl_session_tickets off;
-		ssl_stapling on;
-		ssl_stapling_verify on;
-		#ssl_dhparam /etc/nginx/nginx.pem;
+	ssl_certificate       /etc/trojan/trojan.crt;
+	ssl_certificate_key   /etc/trojan/trojan.key;
+	ssl_protocols         TLSv1.3 TLSv1.2;
+	ssl_ciphers $cipher_server;
+	ssl_prefer_server_ciphers on;
+	ssl_early_data on;
+	ssl_session_cache   shared:SSL:40m;
+	ssl_session_timeout 1d;
+	ssl_session_tickets off;
+	ssl_stapling on;
+	ssl_stapling_verify on;
+	#ssl_dhparam /etc/nginx/nginx.pem;
 
-		resolver 1.1.1.1;
-		resolver_timeout 10s;
-		server_name           $domain;
-		#add_header alt-svc 'quic=":443"; ma=2592000; v="46"';
-		add_header X-Frame-Options SAMEORIGIN always;
-		add_header X-Content-Type-Options "nosniff" always;
-		add_header X-XSS-Protection "1; mode=block" always;
-		add_header Referrer-Policy "no-referrer";
-		add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
-		#add_header Content-Security-Policy "default-src 'self'; script-src 'self' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
-		#add_header Feature-Policy "geolocation none;midi none;notifications none;push none;sync-xhr none;microphone none;camera none;magnetometer none;gyroscope none;speaker self;vibrate none;fullscreen self;payment none;";
-		if (\$http_user_agent = "") { return 444; }
-		if (\$host != "$domain") { return 404; }
-				location / {
-						root /usr/share/nginx/html;
-						index index.html;
-				}
+	resolver 1.1.1.1;
+	resolver_timeout 10s;
+	server_name           $domain;
+	#add_header alt-svc 'quic=":443"; ma=2592000; v="46"';
+	add_header X-Frame-Options SAMEORIGIN always;
+	add_header X-Content-Type-Options "nosniff" always;
+	add_header X-XSS-Protection "1; mode=block" always;
+	add_header Referrer-Policy "no-referrer";
+	add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+	#add_header Content-Security-Policy "default-src 'self'; script-src 'self' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
+	#add_header Feature-Policy "geolocation none;midi none;notifications none;push none;sync-xhr none;microphone none;camera none;magnetometer none;gyroscope none;speaker self;vibrate none;fullscreen self;payment none;";
+	if (\$http_user_agent = "") { return 444; }
+	if (\$host != "$domain") { return 404; }
+	location / {
+		root /usr/share/nginx/html;
+		index index.html;
+	}
 EOF
 fi
 if [[ $install_v2ray = 1 ]]; then
