333ff82ee46ffceb07a6f75fbfb230faf4889b18
johnrosen <48143913+johnrosen1@users.noreply.github.com>
2020-03-18 11:28:43 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -429,7 +429,7 @@ whiptail --clear --ok-button "吾意已決 立即執行" --backtitle "hi 请谨�
 "3" "安裝Trojan-GFW" on \
 "4" "安裝Dnscrypt-proxy | DNS加密(dns encryption)" on \
 "下载" "Download" on  \
-"5" "安裝Qbittorrent | BT客户端(Bittorrent Client)" on \
+"5" "安裝Qbittorrent | BT客户端(Bittorrent Client)" off \
 "6" "安裝Bittorrent-Tracker" on \
 "7" "安裝Aria2" on \
 "8" "安裝Filebrowser | 网盘(File manager)" on \
