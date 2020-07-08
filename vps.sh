351876759d4b23199b1b1fe6c0b79b03cd7f6613
johnrosen <48143913+johnrosen1@users.noreply.github.com>
2020-07-08 16:11:42 +0800

    optimize bbr

--- vps.sh
+++ vps.sh
@@ -1137,6 +1137,7 @@ net.ipv4.tcp_slow_start_after_idle = 0
 net.ipv4.tcp_max_syn_backlog = 30000
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_notsent_lowat = 16384
 net.ipv4.tcp_ecn = 2
 net.ipv4.tcp_ecn_fallback = 1
 net.ipv4.tcp_frto = 0
