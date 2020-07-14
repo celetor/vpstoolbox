5f4d604d745e2f604d74dea61895431439e3ab5b
johnrosen <48143913+johnrosen1@users.noreply.github.com>
2020-07-14 21:37:49 +0800

    block more bad browsers

--- vps.sh
+++ vps.sh
@@ -3427,7 +3427,7 @@ server {
 	
 	resolver 127.0.0.1;
 	resolver_timeout 10s;
-	if (\$http_user_agent ~* (360|Tencent|MicroMessenger|MetaSr|Xiaomi) ) { return 403; }
+	if (\$http_user_agent ~* (360|Tencent|MicroMessenger|MetaSr|Xiaomi|Maxthon|TheWorld|QQ|UC|OPPO|baidu|Sogou|2345) ) { return 403; }
 	#if (\$http_user_agent ~* (wget|curl) ) { return 403; }
 	#if (\$http_user_agent = "") { return 403; }
 	#if (\$host != "$domain") { return 404; }
@@ -3483,7 +3483,7 @@ server {
 	add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
 	#add_header Content-Security-Policy "default-src 'self'; script-src 'self' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
 	#add_header Feature-Policy "geolocation none;midi none;notifications none;push none;sync-xhr none;microphone none;camera none;magnetometer none;gyroscope none;speaker self;vibrate none;fullscreen self;payment none;";
-	if (\$http_user_agent ~* (360|Tencent|MicroMessenger|MetaSr|Xiaomi) ) { return 403; }
+	if (\$http_user_agent ~* (360|Tencent|MicroMessenger|MetaSr|Xiaomi|Maxthon|TheWorld|QQ|UC|OPPO|baidu|Sogou|2345) ) { return 403; }
 	#if (\$http_user_agent ~* (wget|curl) ) { return 403; }
 	#if (\$http_user_agent = "") { return 403; }
 	#if (\$host != "$domain") { return 404; }
