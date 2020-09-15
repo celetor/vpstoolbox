1d8aaec374fc4061163625bcd5859a4f7c2ffc64
johnrosen <john@johnrosen.xyz>
2020-09-15 23:38:45 +0800

    minor optimization for mail service

--- vps.sh
+++ vps.sh
@@ -3137,6 +3137,13 @@ if [[ $install_mail = 1 ]]; then
 	adduser postfix opendmarc
 	systemctl restart opendmarc
 	echo ${domain} > /etc/mailname
+  postproto="ipv4"
+  if [[ -n $myipv6 ]]; then
+    ping -6 ipv6.google.com -c 2 || ping -6 2620:fe::10 -c 2
+    if [[ $? -eq 0 ]]; then
+     postproto="all"
+    fi
+  fi
 	cat > '/etc/postfix/main.cf' << EOF
 home_mailbox = Maildir/
 smtpd_banner = \$myhostname ESMTP \$mail_name (Debian/GNU)
@@ -3182,7 +3189,7 @@ mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 ${myip}/32
 mailbox_size_limit = 0
 recipient_delimiter = +
 inet_interfaces = all
-inet_protocols = all
+inet_protocols = ${postproto}
 message_size_limit = 52428800
 smtpd_helo_required = yes
 disable_vrfy_command = yes
