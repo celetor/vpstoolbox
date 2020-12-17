29055b6ce408b3bceb6edf69bdf76a75f3bf8176
johnrosen <john@johnrosen1.com>
2020-12-17 10:34:22 +0800

    minor optimization

--- vps.sh
+++ vps.sh
@@ -4780,6 +4780,7 @@ net.ipv4.tcp_frto = 0
 net.ipv6.conf.all.accept_redirects = 0
 net.ipv6.conf.default.accept_redirects = 0
 vm.swappiness = 1
+vm.overcommit_memory = 1
 net.ipv4.neigh.default.gc_thresh3=8192
 net.ipv4.neigh.default.gc_thresh2=4096
 net.ipv4.neigh.default.gc_thresh1=2048
@@ -4788,6 +4789,7 @@ net.ipv6.neigh.default.gc_thresh2=4096
 net.ipv6.neigh.default.gc_thresh1=2048
 EOF
   sysctl --system
+  echo madvise > /sys/kernel/mm/transparent_hugepage/enabled
   cat > '/etc/systemd/system.conf' << EOF
 [Manager]
 #DefaultTimeoutStartSec=90s
