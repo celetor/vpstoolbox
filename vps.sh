43f8aaeef00a5c3554af9165ad8c1b920708c234
johnrosen <john@johnrosen1.com>
2020-12-21 18:14:28 +0800

    minor optimization

--- vps.sh
+++ vps.sh
@@ -5132,7 +5132,7 @@ net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_notsent_lowat = 16384
 net.ipv4.tcp_no_metrics_save = 1
-net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_ecn = 2
 net.ipv4.tcp_ecn_fallback = 1
 net.ipv4.tcp_frto = 0
 ##############################
