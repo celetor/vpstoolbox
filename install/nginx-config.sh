9e469a8dfab67b1c29a5ced070363771ba25a6c8
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2021-03-20 11:12:47 +0800

    minor optimization

--- install/nginx-config.sh
+++ install/nginx-config.sh
@@ -26,8 +26,8 @@ server {
   add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
   add_header X-Cache-Status \$upstream_cache_status;
   location / {
-    proxy_pass http://127.0.0.1:4000/;
-    #proxy_pass http://127.0.0.1:4000/public/;
+    #proxy_pass http://127.0.0.1:4000/;
+    root /usr/share/nginx/hexo/public/;
     #error_page 404  /404.html;
     http2_push /css/main.css;
         http2_push /lib/font-awesome/css/all.min.css;
