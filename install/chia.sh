fbe161e90e60e5c375dd6f8c8d0635cbc6be32ed
johnrosen1 <48143913+johnrosen1@users.noreply.github.com>
2021-04-25 20:27:32 +0800

    新增自建Chia节点功能

---
+++ install/chia.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+## Chia Fullnode模组 Chia Fullnode moudle
+
+#---Author Info---
+ver="1.0.0"
+Author="johnrosen1"
+url="https://johnrosen1.com/"
+github_url="https://github.com/johnrosen1/vpstoolbox"
+#-----------------
+
+install_chia() {
+    apt-get install git -y
+    git clone https://github.com/Chia-Network/chia-blockchain.git -b latest
+    cd chia-blockchain
+    sh install.sh
+    . ./activate
+    chia init
+    chia keys generate
+    chia start node
+}
+install_chia
