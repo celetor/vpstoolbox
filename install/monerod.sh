9fff6f6b1770ac4ab5bb560d17c81a3810d5f8c1
johnrosen <48143913+johnrosen1@users.noreply.github.com>
2021-05-03 11:07:52 +0800

    bug fix

--- install/monerod.sh
+++ install/monerod.sh
@@ -11,7 +11,7 @@ github_url="https://github.com/johnrosen1/vpstoolbox"
 
 install_monerod() {
 curl -LO https://downloads.getmonero.org/linux64
-tar -xjvf linux64 -C monero
+tar -xjvf linux64
 mv monero-x86_64* monero
 cd monero
 screen -d -m ./monerod --restricted-rpc --rpc-bind-ip 0.0.0.0 --rpc-bind-port 18081 --confirm-external-bind --rpc-ssl enabled
