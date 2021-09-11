cf80c261f876ad069be266b15399c827428be9c2
杰森 <48143913+johnrosen1@users.noreply.github.com>
2021-09-11 18:38:42 +0800

    update ss-rust version

--- install/ss-rust.sh
+++ install/ss-rust.sh
@@ -10,7 +10,7 @@ github_url="https://github.com/johnrosen1/vpstoolbox"
 #-----------------
 
 install_ss_rust(){
-	curl -LO https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.8.23/shadowsocks-v1.8.23.x86_64-unknown-linux-gnu.tar.xz
+	curl -LO https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.11.2/shadowsocks-v1.11.2.x86_64-unknown-linux-gnu.tar.xz
 	tar -xvf shadowsocks*
 	cp -f ssserver /usr/sbin
 	mkdir /etc/ss-rust
