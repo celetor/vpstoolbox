5a7d4e5dbceee038d2188ba351ace61fff03776c
杰森 <48143913+johnrosen1@users.noreply.github.com>
2021-09-16 23:37:46 +0800

    更新Debian apt sources list

--- install/uninstall-aegis.sh
+++ install/uninstall-aegis.sh
@@ -3,19 +3,11 @@
 ## 阿里云云盾卸载模组 Uninstall Aliyun-aegis moudle
 
 uninstall_aegis(){
+
+set +e
+
 TERM=ansi whiptail --title "阿里云监控卸载" --infobox "检测到阿里云恶意监控服务，开始卸载..." 7 68
 colorEcho ${INFO} "Uninstall Aliyun aegis ing"
-iptables -I INPUT -s 140.205.201.0/28 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.201.16/29 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.201.32/28 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.192/29 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.200/30 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.184/29 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.183/32 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.206/32 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.205/32 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.195/32 -j DROP &>/dev/null
-iptables -I INPUT -s 140.205.225.204/32 -j DROP &>/dev/null
 systemctl stop aegis
 systemctl stop CmsGoAgent.service
 systemctl stop aliyun
@@ -71,18 +63,14 @@ curl -LO https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/install/
 
 if [[ $(lsb_release -cs) == buster ]]; then
       cat > '/etc/apt/sources.list' << EOF
-#------------------------------------------------------------------------------#
-#                   OFFICIAL DEBIAN REPOS                    
-#------------------------------------------------------------------------------#
-###### Debian Main Repos
-deb http://deb.debian.org/debian/ stable main contrib non-free
-#deb-src http://deb.debian.org/debian/ stable main contrib non-free
-deb http://deb.debian.org/debian/ stable-updates main contrib non-free
-#deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free
-deb http://deb.debian.org/debian-security stable/updates main
-#deb-src http://deb.debian.org/debian-security stable/updates main
-deb http://ftp.debian.org/debian buster-backports main
-#deb-src http://ftp.debian.org/debian buster-backports main
+deb http://ftp.us.debian.org/debian/ buster main contrib non-free
+#deb-src http://ftp.us.debian.org/debian/ buster main contrib non-free
+
+deb http://ftp.us.debian.org/debian/ buster-updates main contrib non-free
+#deb-src http://ftp.us.debian.org/debian/ buster-updates main contrib non-free
+
+deb http://security.debian.org/ buster/updates main contrib non-free
+#deb-src http://security.debian.org/ buster/updates main contrib non-free
 EOF
 fi
 
