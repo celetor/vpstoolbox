f78ec61b61055f4e425e2f8e80112a126562d849
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-01-24 20:36:10 +0800

    bug fix

--- install/docker.sh
+++ install/docker.sh
@@ -30,5 +30,10 @@ fi
   "experimental" : true
 }
 EOF
+## Install Docker Compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 systemctl restart docker
 }
