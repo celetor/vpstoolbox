e8ae148d308b16fe15804b80a20c5c9836f4d34b
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-01-29 08:49:10 +0800

    update qbt url

--- install/qbt.sh
+++ install/qbt.sh
@@ -12,7 +12,7 @@ cd
 mkdir qbt
 cd qbt
 qbtver=$(curl -s "https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-wget https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/${qbtver}/qbittorrent-nox_x86_64-linux-musl_static.zip
+wget https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/${qbtver}/qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip
 unzip -o qbittorrent*.zip
 cp -f qbittorrent-nox /usr/bin/
 chmod +x /usr/bin/qbittorrent-nox
