bce24f34aab9c4bd3f52c42cad2f57411ac7ecb4
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-02-14 13:18:38 +0800

    bug fix

--- install/netdata.sh
+++ install/netdata.sh
@@ -51,32 +51,32 @@ local:
   url     : 'http://127.0.0.1:83/status?full&json'
 EOF
 fi
-if [[ ${install_tor} == 1 ]]; then
-apt-get install python-pip -y
-pip install stem
-cat > '/opt/netdata/etc/netdata/python.d/tor.conf' << EOF
-update_every : 1
-priority     : 60001
-
-local_tcp:
- name: 'local'
- control_port: 9051
-EOF
-fi
-if [[ ${install_redis} == 1 ]]; then
-cat > '/opt/netdata/etc/netdata/python.d/redis.conf' << EOF
-socket:
-  name     : 'local'
-  socket   : '/var/run/redis/redis.sock'
-EOF
-fi
-cat > '/opt/netdata/etc/netdata/python.d/mysql.conf' << EOF
-update_every : 10
-priority     : 90100
-
-local:
-  user     : 'netdata'
-  update_every : 1
+#if [[ ${install_tor} == 1 ]]; then
+#apt-get install python-pip -y
+#pip install stem
+#cat > '/opt/netdata/etc/netdata/python.d/tor.conf' << EOF
+#update_every : 1
+#priority     : 60001
+#
+#local_tcp:
+# name: 'local'
+# control_port: 9051
+#EOF
+#fi
+#if [[ ${install_redis} == 1 ]]; then
+#cat > '/opt/netdata/etc/netdata/python.d/redis.conf' << EOF
+#socket:
+#  name     : 'local'
+#  socket   : '/var/run/redis/redis.sock'
+#EOF
+#fi
+#cat > '/opt/netdata/etc/netdata/python.d/mysql.conf' << EOF
+#update_every : 10
+#priority     : 90100
+#
+#local:
+#  user     : 'netdata'
+#  update_every : 1
 EOF
 systemctl restart netdata
 clear
