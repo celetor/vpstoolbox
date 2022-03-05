74d7ba0d799d472a23cbe4e2673646b529514165
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-05 20:33:50 +0800

    Update aria2.sh

--- install/aria2.sh
+++ install/aria2.sh
@@ -30,7 +30,7 @@ sed -i 's/PREF_CONNECT_TIMEOUT, TEXT_CONNECT_TIMEOUT, "60", 1, 600/PREF_CONNECT_
 sed -i 's/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "1M", 1_m, 1_g/PREF_PIECE_LENGTH, TEXT_PIECE_LENGTH, "4k", 1_k, 1_g/g' /root/aria2*/src/OptionHandlerFactory.cc
 sed -i 's/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "0", 0, 600/new NumberOptionHandler(PREF_RETRY_WAIT, TEXT_RETRY_WAIT, "2", 0, 600/g' /root/aria2*/src/OptionHandlerFactory.cc
 sed -i 's/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "5", 1, -1,/new NumberOptionHandler(PREF_SPLIT, TEXT_SPLIT, "8", 1, -1,/g' /root/aria2*/src/OptionHandlerFactory.cc
-/root/aria2*/configure
+configure
 make -j $(nproc --all)
 make install
 chmod +x /usr/local/bin/aria2c
