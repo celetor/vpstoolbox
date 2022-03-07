1a990783b0f52965233bb811c10dd8cfa28fbf4a
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-07 13:34:31 +0800

    Update jellyfin.sh

--- install/jellyfin.sh
+++ install/jellyfin.sh
@@ -19,7 +19,6 @@ wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 apt-get update
 apt-get install -y jellyfin
-
 sed -i "s/<BaseUrl \/>/<BaseUrl>\/jellyfin\/<\/BaseUrl>/g" /etc/jellyfin/system.xml
 sed -i "s/<EnableIPV6>false<\/EnableIPV6>/<EnableIPV6>true<\/EnableIPV6>/g" /etc/jellyfin/system.xml
 sed -i "s/<EnableRemoteAccess>true<\/EnableRemoteAccess>/<EnableRemoteAccess>false<\/EnableRemoteAccess>/g" /etc/jellyfin/system.xml
@@ -27,8 +26,6 @@ sed -i "s/en-US/zh-CN/g" /etc/jellyfin/system.xml
 sed -i "s/<BaseUrl \/>/<BaseUrl>\/jellyfin\/<\/BaseUrl>/g" /etc/jellyfin/network.xml
 sed -i "s/<EnableIPV6>false<\/EnableIPV6>/<EnableIPV6>true<\/EnableIPV6>/g" /etc/jellyfin/network.xml
 sed -i "s/<IgnoreVirtualInterfaces>true<\/IgnoreVirtualInterfaces>/<IgnoreVirtualInterfaces>false<\/IgnoreVirtualInterfaces>/g" /etc/jellyfin/network.xml
-
 systemctl restart jellyfin
-
 cd
 }
