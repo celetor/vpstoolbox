dcae33c58fb234cfad42179e01c71d5e599f43e0
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-07 12:03:36 +0800

    Update userinput.sh

--- install/userinput.sh
+++ install/userinput.sh
@@ -99,7 +99,7 @@ userinput_standard() {
   #echo "$(jq -r '.ip' "/root/.trojan/ip.json") ${domain}" >> /etc/hosts
   if [[ ${install_trojan} = 1 ]]; then
     while [[ -z ${password1} ]]; do
-      password1=$(whiptail --passwordbox --nocancel "Trojan-GFW 密码一 (推荐自定义密码,***请勿添加特殊符号***)" 8 68 --title "password1 input" 3>&1 1>&2 2>&3)
+      password1=$(whiptail --passwordbox --nocancel "Trojan-GFW 密码 (***请勿添加特殊符号***)" 8 68 --title "password1 input" 3>&1 1>&2 2>&3)
       if [[ -z ${password1} ]]; then
         password1=$(
           head /dev/urandom | tr -dc a-z0-9 | head -c 9
@@ -374,7 +374,7 @@ userinput_full() {
   #echo "$(jq -r '.ip' "/root/.trojan/ip.json") ${domain}" >> /etc/hosts
   if [[ ${install_trojan} = 1 ]]; then
     while [[ -z ${password1} ]]; do
-      password1=$(whiptail --passwordbox --nocancel "Trojan-GFW 密码一 (推荐自定义密码,***请勿添加特殊符号***)" 8 68 --title "password1 input" 3>&1 1>&2 2>&3)
+      password1=$(whiptail --passwordbox --nocancel "Trojan-GFW 密码 (***请勿添加特殊符号***)" 8 68 --title "password1 input" 3>&1 1>&2 2>&3)
       if [[ -z ${password1} ]]; then
         password1=$(
           head /dev/urandom | tr -dc a-z0-9 | head -c 9
