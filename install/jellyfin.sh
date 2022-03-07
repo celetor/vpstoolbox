400302398d62d346d562bb4055b765d7687f4ac2
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-07 10:50:31 +0800

    Update jellyfin.sh

--- install/jellyfin.sh
+++ install/jellyfin.sh
@@ -14,9 +14,44 @@ github_url="https://github.com/johnrosen1/vpstoolbox"
 #-----------------
 
 install_jellyfin(){
-apt install -y apt-transport-https
-wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
-echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
-apt-get update
-apt-get install -y jellyfin
+#apt install -y apt-transport-https
+#wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
+#echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+#apt-get update
+#apt-get install -y jellyfin
+
+## Docker install
+
+cd /usr/share/nginx/
+mkdir jellyfin
+cd /usr/share/nginx/jellyfin
+mkdir /usr/share/nginx/jellyfin/config
+mkdir /usr/share/nginx/jellyfin/cache
+mkdir /usr/share/nginx/jellyfin/media
+
+    cat > "docker-compose.yml" << "EOF"
+version: "3.5"
+services:
+  jellyfin:
+    image: jellyfin/jellyfin
+    container_name: jellyfin
+    user: uid:gid
+    network_mode: "host"
+    volumes:
+      - /usr/share/nginx/jellyfin/config:/config
+      - /usr/share/nginx/jellyfin/cache:/cache
+      - /usr/share/nginx/jellyfin/media:/media
+      - /usr/share/nginx/:/media2:ro
+    restart: "unless-stopped"
+    # Optional - alternative address used for autodiscovery
+    environment:
+      - JELLYFIN_PublishedServerUrl=http://example.com
+
+EOF
+sed -i "s/http/https/g" docker-compose.yml
+sed -i "s/example.com/${domain}\/jellyfin\//g" docker-compose.yml
+
+docker-compose up -d
+
+cd
 }
