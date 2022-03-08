de6e764d8dde887f4b55a6cfb9d383d64b11eb5f
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-08 23:14:56 +0800

    Update output.sh

--- install/output.sh
+++ install/output.sh
@@ -96,7 +96,7 @@ echo -e " --- \${BLUE}帶寬使用(Bandwith Usage)\${NOCOLOR} ---"
 echo -e "         接收(Receive)    发送(Transmit)"
 tail -n +3 /proc/net/dev | awk '{print \$1 " " \$2 " " \$10}' | numfmt --to=iec --field=2,3
 echo -e " --- \${GREEN}證書狀態(Certificate Status)\${NOCOLOR} ---"
-ssl_date=\$(echo |openssl s_client -connect ${domain}:${trojanport} -tls1_3 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'|openssl x509 -text)
+ssl_date=\$(echo |timeout 3 openssl s_client -connect ${domain}:${trojanport} -tls1_3 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'|openssl x509 -text)
 tmp_last_date=\$(echo "\${ssl_date}" | grep 'Not After :' | awk -F' : ' '{print \$NF}')
 last_date=\$(date -ud "\${tmp_last_date}" +%Y-%m-%d" "%H:%M:%S)
 day_count=\$(( (\$(date -d "\${last_date}" +%s) - \$(date +%s))/(24*60*60) ))
