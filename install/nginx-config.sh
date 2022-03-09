d4c0684687a1336b6c6fcea737fa907a4ee8beeb
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-09 13:10:53 +0800

    Update nginx-config.sh

--- install/nginx-config.sh
+++ install/nginx-config.sh
@@ -131,6 +131,7 @@ cat << EOF > /etc/nginx/conf.d/grpc.conf
 	location /${uuid_new} {
 		client_max_body_size 0;
 		grpc_set_header X-Real-IP \$proxy_add_x_forwarded_for;
+    grpc_socket_keepalive on;
     #keepalive_timeout 1071906480m;
 		client_body_timeout 1071906480m;
 		grpc_read_timeout 1071906480m;
