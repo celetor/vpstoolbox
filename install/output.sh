ff0aa7d31891acea2d324ebe30959cdde4dc7083
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-10 13:54:32 +0800

    Update output.sh

--- install/output.sh
+++ install/output.sh
@@ -102,7 +102,7 @@ echo -e "\e[40;33;1m [${domain}] 证书过期日期 : \${last_date} && [\${day_c
 echo -e "------------------------------------------------------------------------"
 if [[ -f /usr/bin/xray ]]; then
 echo -e " --- \${BLUE}Vless(grpc)链接(推荐使用 支持Cloudflare CDN)\${NOCOLOR} ---"
-echo -e "    \${YELLOW}vless://${uuid_new}@${domain}:${trojanport}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=/${uuid_new}&sni=${domain}#vless(grpc_cdn)\${NOCOLOR}"
+echo -e "    \${YELLOW}vless://${uuid_new}@${domain}:${trojanport}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=/${uuid_new}&sni=${domain}#Vless(grpc_cdn)\${NOCOLOR}"
 echo -e " --- \${BLUE}Trojan(grpc)链接(支持Cloudflare CDN)\${NOCOLOR} ---"
 echo -e "    \${YELLOW}trojan://${uuid_new}@${domain}:${trojanport}?encryption=none&security=tls&type=grpc&sni=${domain}&alpn=h2&serviceName=/${uuid_new}_trojan#Trojan(grpc_cdn)\${NOCOLOR}"
 echo -e " --- \${BLUE}Trojan-GFW链接(不支持Cloudflare CDN)\${NOCOLOR} ---"
