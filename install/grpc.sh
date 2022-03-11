de6ce61a989c46f8e6d3bb79569d6aa7fb041999
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-11 13:20:59 +0800

    Update grpc.sh

--- install/grpc.sh
+++ install/grpc.sh
@@ -100,7 +100,7 @@ cd /etc/grpc
 EOF
 
 uuid_new=$(/usr/bin/xray uuid -i "${password1}")
-path_new=$(/usr/bin/xray uuid -i "${password1}" | cut -c1-8 )
+path_new=$(/usr/bin/xray uuid -i "${password1}" | cut -c1-6 )
 sed -i "s/49ec002d-ca69-4325-aea5-dbef18dd6f42/${uuid_new}/g" server.json
 sed -i "s/\/grpc/\/${path_new}/g" server.json
 
