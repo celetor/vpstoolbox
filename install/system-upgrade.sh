864d8ce676b9edbf4cd4023427e9fdabfcd21b5c
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-11 13:33:51 +0800

    Update system-upgrade.sh

--- install/system-upgrade.sh
+++ install/system-upgrade.sh
@@ -7,16 +7,7 @@ set +e
 upgrade_system(){
 
 ## Install lastest ubuntu lts
-if [[ $(lsb_release -cs) == bionic ]] || [[ $(lsb_release -cs) == xenial ]] || [[ $(lsb_release -cs) == trusty ]]; then
-  ubuntu18_install=1
-fi
-## Install lastest stable debian
-if [[ $(lsb_release -cs) == stretch ]] || [[ $(lsb_release -cs) == buster ]] || [[ $(lsb_release -cs) == jessie ]]; then
-  debian10_install=1
-fi
-
- if [[ $dist == ubuntu ]]; then
-  if [[ $ubuntu18_install == 1 ]]; then
+if [[ $dist == ubuntu ]]; then
     cat > '/etc/apt/sources.list' << EOF
 deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse
@@ -24,7 +15,6 @@ deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe mu
 deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse
 deb http://archive.canonical.com/ubuntu focal partner
 EOF
-fi
   apt-get update --fix-missing
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y'
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y'
@@ -32,16 +22,15 @@ fi
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y'
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get autoremove -qq -y'
   clear
- elif [[ $dist == debian ]]; then
+else
+## Install lastest stable debian
   apt-get update --fix-missing
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get upgrade -y'
-  if [[ ${debian10_install} == 1 ]]; then
     cat > '/etc/apt/sources.list' << EOF
 deb http://deb.debian.org/debian/ stable main contrib non-free
 deb http://deb.debian.org/debian/ stable-updates main contrib non-free
 deb http://deb.debian.org/debian-security stable-security main
 EOF
-fi
   apt-get update --fix-missing
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y'
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -q -y'
@@ -49,9 +38,5 @@ fi
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y'
   sh -c 'echo "y\n\ny\ny\ny\ny\ny\ny\ny\n" | DEBIAN_FRONTEND=noninteractive apt-get autoremove -qq -y'
   clear
- else
-  clear
-  TERM=ansi whiptail --title "error can't upgrade system" --infobox "error can't upgrade system" 8 68
-  exit 1;
- fi
+fi
 }
