749ab6e995d181af25112cf253eae7b4ac756a15
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-12 11:42:50 +0800

    Update qbt.sh

--- install/qbt.sh
+++ install/qbt.sh
@@ -89,6 +89,9 @@ curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22max_connec_per_t
 curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22web_ui_address%22:%22127.0.0.1%22%7D  --cookie "${qbtcookie}"
 curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22validate_https_tracker_certificate%22:false%7D  --cookie "${qbtcookie}"
 curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22peer_tos%22:0%7D  --cookie "${qbtcookie}"
+## 设置自动管理以及默认下载位置
+curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22auto_tmm_enabled%22:true%7D  --cookie "${qbtcookie}"
+curl http://localhost:8080/api/v2/app/setPreferences?json=%7B%22save_path%22:%22%2fusr%2fshare%2fnginx%2fdata%2ftorrents%2f%22%7D  --cookie "${qbtcookie}"
 ## 新增分类以及下载位置
 mkdir /usr/share/nginx/data/
 mkdir /usr/share/nginx/data/torrents/
