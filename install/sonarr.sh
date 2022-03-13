3d1d31d8a8c997d34030953e1839c2a2912fd4b5
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-13 23:36:43 +0800

    Update sonarr.sh

--- install/sonarr.sh
+++ install/sonarr.sh
@@ -418,7 +418,6 @@ sed -i '/^\[analytics\]$/,/^\[/ s/^enabled = True/enabled = False/' /usr/share/n
 sed -i "s/use_sonarr = False/use_sonarr = True/g" /usr/share/nginx/bazarr/config/config/config.ini
 sed -i "s/use_radarr = False/use_radarr = True/g" /usr/share/nginx/bazarr/config/config/config.ini
 sed -i "s/enabled_providers = \[\]/enabled_providers = \['zimuku', 'subscenter'\]/g" /usr/share/nginx/bazarr/config/config/config.ini
-## enabled_providers = ['zimuku']
 sqlite3 /usr/share/nginx/bazarr/config/db/bazarr.db  "insert into table_languages_profiles values ('1','','[{\"id\": 1, \"language\": \"zh\", \"audio_exclude\": \"False\", \"hi\": \"False\", \"forced\": \"False\"}, {\"id\": 2, \"language\": \"zt\", \"audio_exclude\": \"False\", \"hi\": \"False\", \"forced\": \"False\"}, {\"id\": 3, \"language\": \"en\", \"audio_exclude\": \"False\", \"hi\": \"False\", \"forced\": \"False\"}]','Chinese','[]','[]');"
 docker-compose up -d
 cd
