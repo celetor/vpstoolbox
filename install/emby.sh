b99d4d8cda9dbcd8eb88330a0dfb9fa881822f19
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-13 13:34:07 +0800

    Update emby.sh

--- install/emby.sh
+++ install/emby.sh
@@ -15,4 +15,13 @@ install_emby(){
 apt install -y apt-transport-https
 curl -LO https://github.com/MediaBrowser/Emby.Releases/releases/download/4.6.7.0/emby-server-deb_4.6.7.0_amd64.deb
 dpkg -i emby-server-deb_4.6.7.0_amd64.deb
+
+cat /etc/emby-server.conf | grep media &> /dev/null
+
+if [[ $? != 0 ]]; then
+systemctl stop emby-server
+sed -i "s/EMBY_DATA=/EMBY_DATA=\/usr\/share\/nginx\/data\/media\//g" /etc/emby-server.conf
+systemctl restart emby-server
+fi
+cd /root
 }
