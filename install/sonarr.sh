5f4c89f34fd1ea06208a9b000ac12333cb176ec5
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-22 12:51:40 +0800

    Update sonarr.sh

--- install/sonarr.sh
+++ install/sonarr.sh
@@ -560,7 +560,7 @@ sed -i "s/FormAuth=no/FormAuth=yes/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/AuthorizedIP=127.0.0.1/AuthorizedIP=/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/ControlPassword=tegbzn6789/ControlPassword=${password1}/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/Server1.Port=119/Server1.Port=563/g" /usr/share/nginx/nzbget/config/nzbget.conf
-sed -i "s/Server1.Cipher=/Server1.Cipher=AES128-SHA/g" /usr/share/nginx/nzbget/config/nzbget.conf
+sed -i "s/Server1.Cipher=/Server1.Cipher=ECDHE-ECDSA-AES128-GCM-SHA256/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/Server1.Encryption=no/Server1.Encryption=yes/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/Server1.Connections=4/Server1.Connections=50/g" /usr/share/nginx/nzbget/config/nzbget.conf
 sed -i "s/UrlConnections=4/UrlConnections=2/g" /usr/share/nginx/nzbget/config/nzbget.conf
