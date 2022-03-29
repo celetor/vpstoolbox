57b7a7524a00677b5d719d68bcde71594f7d7e0c
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-29 14:28:59 +0800

    Update qbt.sh

--- install/qbt.sh
+++ install/qbt.sh
@@ -111,7 +111,7 @@ curl http://127.0.0.1:8080/api/v2/app/setPreferences?json=%7B%22validate_https_t
 curl http://127.0.0.1:8080/api/v2/app/setPreferences?json=%7B%22peer_tos%22:0%7D  --cookie "${qbtcookie}"
 ## 设置自动管理以及默认下载位置
 curl http://127.0.0.1:8080/api/v2/app/setPreferences?json=%7B%22auto_tmm_enabled%22:true%7D  --cookie "${qbtcookie}"
-curl http://127.0.0.1:8080/api/v2/app/setPreferences?json=%7B%22save_path%22:%22%2fdata%2fmedia%2f%22%7D --cookie "${qbtcookie}"
+curl http://127.0.0.1:8080/api/v2/app/setPreferences?json=%7B%22save_path%22:%22%2fdata%2ftorrents%2f%22%7D --cookie "${qbtcookie}"
 
 ## 新增分类
 curl -X POST -F 'category=Series' -F 'savePath=/data/torrents/Series/' http://127.0.0.1:8080/api/v2/torrents/createCategory --cookie "${qbtcookie}"
