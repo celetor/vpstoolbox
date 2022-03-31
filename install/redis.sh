ac2be9e139992f2b4e17370458d01a141a6ac4f6
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-03-31 11:25:12 +0800

    Update redis.sh

--- install/redis.sh
+++ install/redis.sh
@@ -8,9 +8,7 @@ install_redis(){
   cd
   TERM=ansi whiptail --title "安装中" --infobox "安装redis中..." 7 68
   curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-
   echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
-
   apt-get update
   apt-get install redis -y
   systemctl enable redis-server
