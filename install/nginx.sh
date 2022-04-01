80cbf45cf7e295bc0fc5b9aa78e31966a73f7a98
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-01 15:35:14 +0800

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -59,7 +59,7 @@ error_log /var/log/nginx/error.log warn;
 pid /run/nginx.pid;
 include /etc/nginx/modules-enabled/*.conf;
 events {
-  worker_connections 65536;
+  worker_connections 1000000;
   use epoll;
   multi_accept on;
 }
