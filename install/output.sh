14986b2553d2228f9224268a25c0213f7f3172e3
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-02 17:25:57 +0800

    Update output.sh

--- install/output.sh
+++ install/output.sh
@@ -91,7 +91,7 @@ echo -e "Fail2ban:\t\t"\$(systemctl is-active fail2ban)
   fi
 echo -e " --- \${BLUE}帶寬使用(Bandwith Usage)\${NOCOLOR} ---"
 echo -e "         接收(Receive)    发送(Transmit)"
-tail -n +3 /proc/net/dev | grep -e eth -e enp -e eno | awk '{print \$1 " " \$2 " " \$10}' | numfmt --to=iec --field=2,3
+tail -n +3 /proc/net/dev | grep -e eth -e enp -e eno -e ens | awk '{print \$1 " " \$2 " " \$10}' | numfmt --to=iec --field=2,3
 echo -e " --- \${GREEN}證書狀態(Certificate Status)\${NOCOLOR} ---"
 ssl_date=\$(echo |timeout 3 openssl s_client -4 -connect ${myip}:${trojanport} -servername ${domain} -tls1_3 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'|openssl x509 -text) &>/dev/null
 tmp_last_date=\$(echo "\${ssl_date}" | grep 'Not After :' | awk -F' : ' '{print \$NF}')
