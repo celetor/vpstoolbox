f68fabfad3f8b497195983ff09ce2cc59f3ddda8
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-18 14:46:25 +0800

    update nginx config

--- install/nginx.sh
+++ install/nginx.sh
@@ -104,6 +104,9 @@ http {
   proxy_set_header X-Real-IP \$remote_addr;
   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 
+  real_ip_header X-Forwarded-For; 
+  real_ip_recursive on;
+
   include /etc/nginx/mime.types;
   default_type application/octet-stream;
 
