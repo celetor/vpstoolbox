be8d72c8897d4bd12db2a1ae045d27a3d32237e8
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-18 15:07:08 +0800

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -139,7 +139,7 @@ http {
 
   log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
     '\$status \$body_bytes_sent "\$http_referer" '
-    '"\$http_user_agent" "\$http_x_forwarded_for"';
+    '"\$http_user_agent" "\$http_x_forwarded_for" "\$http_cf_connecting_ip" ';
 
   sendfile on;
   gzip on;
