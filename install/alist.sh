cc0d60c21a252f7a7c3b6115884e88ad06950941
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-20 12:28:58 +0800

    add alist

---
+++ install/alist.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Alist模组
+
+set +e
+
+install_alist(){
+    if [[ -d /opt/alist ]]; then
+        curl -fsSL "https://nn.ci/alist.sh" | bash -s update
+    else
+        curl -fsSL "https://nn.ci/alist.sh" | bash -s install
+    fi
+
+    alist_password=$(./alist -password | awk -F'your password: ' '{print $2}' 2>&1)
+
+cat '/opt/alist/data/config.json' | jq '.address |= "127.0.0.1"' >> /opt/alist/data/config.json.tmp
+cp -f /opt/alist/data/config.json.tmp /opt/alist/data/config.json
+rm /opt/alist/data/config.json.tmp
+systemctl restart alist
+}
