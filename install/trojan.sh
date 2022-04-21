c564782891327d3d6a4260c597cd16edad9f42fe
杰森 <48143913+johnrosen1@users.noreply.github.com>
2022-04-21 21:49:11 +0800

    bug fix

--- install/trojan.sh
+++ install/trojan.sh
@@ -99,7 +99,86 @@ systemctl enable trojan
     }
 }
 EOF
+if [[ ${trojanport} != 443 ]]; then
+  cat > '/etc/systemd/system/trojan-web.service' << EOF
+[Unit]
+Description=trojan
+Documentation=https://trojan-gfw.github.io/trojan/config https://trojan-gfw.github.io/trojan/
+After=network.target network-online.target nss-lookup.target mysql.service mariadb.service mysqld.service
+
+[Service]
+Type=simple
+StandardError=journal
+ExecStart=/usr/local/bin/trojan /usr/local/etc/trojan/config-web.json
+ExecReload=/bin/kill -HUP \$MAINPID
+LimitNOFILE=infinity
+Restart=always
+RestartSec=3s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable trojan
+    cat > '/usr/local/etc/trojan/config-web.json' << EOF
+{
+    "run_type": "server",
+    "local_addr": "::",
+    "local_port": 443,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 81,
+    "password": [
+        "$password1",
+        "$password2"
+    ],
+    "log_level": 2,
+    "ssl": {
+        "cert": "/etc/certs/${domain}_ecc/fullchain.cer",
+        "key": "/etc/certs/${domain}_ecc/${domain}.key",
+        "key_password": "",
+        "cipher": "$cipher_server",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+          "h2",
+            "http/1.1"
+        ],
+        "alpn_port_override": {
+            "h2": 82
+        },
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "prefer_ipv4": true,
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": true,
+        "fast_open": true,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": "${password1}",
+        "key": "",
+        "cert": "",
+        "ca": ""
+    }
+}
+EOF
+systemctl daemon-reload
+systemctl restart trojan-web
+fi
 chmod -R 755 /usr/local/etc/trojan/
+systemctl daemon-reload
 systemctl restart trojan
 }
 
